d5b1855b5871d0d6194d02c5f080511517057022
Milan Maragiri <milanmaragiri@ucr-secure-36-10-13-193-106.wnet.ucr.edu>
2025-05-07 18:02:18 -0700

    Implementation of A* with manhattan heuristic

--- main.cpp
+++ main.cpp
@@ -27,6 +27,21 @@ int countMisplacedTiles(const vector<vector<int>>& state) {
     return misplaced;
 }
 
+int manhattanDistance(const vector<vector<int>>& state) {
+    int distance = 0;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            int val = state[i][j];
+            if (val != 0) {
+                int goal_i = (val - 1) / N;
+                int goal_j = (val - 1) % N;
+                distance += abs(i - goal_i) + abs(j - goal_j);
+            }
+        }
+    }
+    return distance;
+}
+
 struct PuzzleState {
     vector<vector<int>> state;
     int g, h, f;
@@ -165,6 +180,49 @@ void misplacedTileSearch(const vector<vector<int>>& initial_state) {
     cout << "Failure\n";
 }
 
+void manhattanDistanceSearch(const vector<vector<int>>& initial_state) {
+    priority_queue<PuzzleState*, vector<PuzzleState*>, CompareUsingF> search_queue;
+    unordered_set<string> explored;
+
+    int h = manhattanDistance(initial_state);
+    search_queue.push(new PuzzleState(initial_state, 0, h));
+
+    int nodes_expanded = 0;
+    int max_queue_size = 1;
+
+    while (!search_queue.empty()) {
+        PuzzleState* node = search_queue.top();
+        search_queue.pop();
+
+        string sig = stateToString(node->state);
+        if (explored.count(sig)) continue;
+        explored.insert(sig);
+
+        cout << "The best state to expand with a g(n) = " << node->g
+             << " and h(n) = " << node->h << " is...\n";
+        printState(node->state);
+        nodes_expanded++;
+
+        if (isGoal(node->state)) {
+            cout << "Goal state!\n";
+            cout << "Solution depth was " << node->g << endl;
+            cout << "Number of nodes expanded: " << nodes_expanded << endl;
+            cout << "Max queue size: " << max_queue_size << endl;
+            return;
+        }
+
+        auto children = expand(node);
+        for (PuzzleState* child : children) {
+            child->h = manhattanDistance(child->state);
+            child->f = child->g + child->h;
+            search_queue.push(child);
+        }
+        max_queue_size = max(max_queue_size, (int)search_queue.size());
+    }
+    cout << "Failure\n";
+}
+
+
 vector<vector<int>> inputPuzzle(const string& label) {
     vector<vector<int>> puzzle(N, vector<int>(N));
     cout << "Enter the " << label << " puzzle row by row (use 0 for blank):\n";
@@ -199,8 +257,8 @@ int main() {
         uniformCostSearch(initial_state);
     else if (algo == 2)
         misplacedTileSearch(initial_state);
-    // else if (algo == 3)
-    //     manhattanDistanceSearch(initial_state);
+    else if (algo == 3)
+        manhattanDistanceSearch(initial_state);
     else
         cout << "Invalid selection.\n";
 }
